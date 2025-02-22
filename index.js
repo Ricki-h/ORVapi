2b18aadac901a79286c96f5eba58b740390952f1
Ricki-h <rick1n.htz@gmail.com>
2025-02-22 20:39:12 -0300

    vercel de novo

--- index.js
+++ index.js
@@ -10,36 +10,36 @@ app.get('/', (req, res) => {
     res.send('ORV API')
 })
 
-// app.post('/characters', async (req, res) => {
-//     await prisma.character.create({
-//         data: {
-//             name: req.body.name,
-//             constellation: req.body.constellation,
-//             race: req.body.race,
-//             img1: req.body.img1,
-//             img2: req.body.img2,
-//             description: req.body.description
-//         }
-//     })
-//     res.status(201).json(req.body)
-// })
+app.post('/characters', async (req, res) => {
+    await prisma.character.create({
+        data: {
+            name: req.body.name,
+            constellation: req.body.constellation,
+            race: req.body.race,
+            img1: req.body.img1,
+            img2: req.body.img2,
+            description: req.body.description
+        }
+    })
+    res.status(201).json(req.body)
+})
 
-// app.put('/characters/:id', async (req, res) => {
-//     await prisma.character.update({
-//         where: {
-//             id: req.params.id
-//         },
-//         data: {
-//             name: req.body.name,
-//             constellation: req.body.constellation,
-//             race: req.body.race,
-//             img1: req.body.img1,
-//             img2: req.body.img2,
-//             description: req.body.description
-//         }
-//     })
-//     res.status(201).json(req.body)
-// })
+app.put('/characters/:id', async (req, res) => {
+    await prisma.character.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            name: req.body.name,
+            constellation: req.body.constellation,
+            race: req.body.race,
+            img1: req.body.img1,
+            img2: req.body.img2,
+            description: req.body.description
+        }
+    })
+    res.status(201).json(req.body)
+})
 
 app.get('/characters', async (req, res) => {
     let characters = []
@@ -57,14 +57,14 @@ app.get('/characters', async (req, res) => {
 })
 
 
-// app.delete('/characters/:id', async (req, res) => {
-//     await prisma.character.delete({
-//         where: {
-//             id: req.params.id
-//         },
-//     })
-//     res.status(200).json({ message: 'Personagem deletado com sucesso!' })
-// })
+app.delete('/characters/:id', async (req, res) => {
+    await prisma.character.delete({
+        where: {
+            id: req.params.id
+        },
+    })
+    res.status(200).json({ message: 'Personagem deletado com sucesso!' })
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
