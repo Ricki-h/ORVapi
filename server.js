45496eb57c09f46f70c1911d43cd6a71d790c06b
Ricki-h <rick1n.htz@gmail.com>
2025-02-22 18:51:02 -0300

    finalizando api

--- server.js
+++ server.js
@@ -16,11 +16,48 @@ app.post('/characters', async (req, res) => {
             description: req.body.description
         }
     })
+    res.status(201).json(req.body)
+})
+
+app.put('/characters/:id', async (req, res) => {
+    await prisma.character.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            name: req.body.name,
+            constellation: req.body.constellation,
+            race: req.body.race,
+            img1: req.body.img1,
+            img2: req.body.img2,
+            description: req.body.description
+        }
+    })
+    res.status(201).json(req.body)
 })
 
 app.get('/characters', async (req, res) => {
-    const characters = await prisma.character.findMany()
+    let characters = []
+    if (req.query) {
+        characters = await prisma.character.findMany({
+            where: {
+                name: req.query.name
+            }
+        })
+    }
+    else {
+        characters = await prisma.character.findMany()
+    }
     res.status(200).json(characters)
+})
+
+app.delete('/characters/:id', async (req, res) => {
+    await prisma.character.delete({
+        where: {
+            id: req.params.id
+        },
+    })
+    res.status(200).json({ message: 'Personagem deletado com sucesso!' })
 })
 
 app.listen(3000)
