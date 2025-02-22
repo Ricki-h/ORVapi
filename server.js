348bb8343cdb7f6574fc13dd74b39a2c3003eb92
Ricki-h <rick1n.htz@gmail.com>
2025-02-22 00:39:31 -0300

    continuando

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 import express, { request } from 'express'
+import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient()
 const app = express()
+app.use(express.json())
 
-app.get('/characters', (req, res) => {
-    res.send('Ok, deu bom')
+app.post('/characters', async (req, res) => {
+    await prisma.character.create({
+        data: {
+            name: req.body.name,
+            constellation: req.body.constellation,
+            race: req.body.race,
+            img1: req.body.img1,
+            img2: req.body.img2,
+            description: req.body.description
+        }
+    })
+})
+
+app.get('/characters', async (req, res) => {
+    const characters = await prisma.character.findMany()
+    res.status(200).json(characters)
 })
 
 app.listen(3000)
