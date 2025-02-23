3e717478de60a3ce358a98a7b87c566f014e889c
Ricki-h <rick1n.htz@gmail.com>
2025-02-22 22:28:39 -0300

    tentando de novo

--- index.js
+++ index.js
@@ -7,40 +7,8 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('ORV API')
+    res.send('ORVapi')
 })
-
-app.post('/characters', async (req, res) => {
-    await prisma.character.create({
-        data: {
-            name: req.body.name,
-            constellation: req.body.constellation,
-            race: req.body.race,
-            img1: req.body.img1,
-            img2: req.body.img2,
-            description: req.body.description
-        }
-    })
-    res.status(201).json(req.body)
-})
-
-app.put('/characters/:id', async (req, res) => {
-    await prisma.character.update({
-        where: {
-            id: req.params.id
-        },
-        data: {
-            name: req.body.name,
-            constellation: req.body.constellation,
-            race: req.body.race,
-            img1: req.body.img1,
-            img2: req.body.img2,
-            description: req.body.description
-        }
-    })
-    res.status(201).json(req.body)
-})
-
 app.get('/characters', async (req, res) => {
     let characters = []
     if (req.query) {
@@ -55,17 +23,38 @@ app.get('/characters', async (req, res) => {
     }
     res.status(200).json(characters)
 })
+app.post('/characters', async (req, res) => {
+    await prisma.character.create({
+        data: {
+            name: req.body.name,
+            constellation: req.body.constellation,
+            race: req.body.race,
+            description: req.body.description,
+            img1: req.body.img1,
+            img2: req.body.img2
+        }
+    })
 
-
-app.delete('/characters/:id', async (req, res) => {
-    await prisma.character.delete({
+    res.status(201).send(req.body)
+})
+app.put('/characters/:id', async (req, res) => {
+    await prisma.character.update({
         where: {
             id: req.params.id
         },
+        data: {
+            name: req.body.name,
+            constellation: req.body.constellation,
+            race: req.body.race,
+            description: req.body.description,
+            img1: req.body.img1,
+            img2: req.body.img2
+        }
     })
-    res.status(200).json({ message: 'Personagem deletado com sucesso!' })
+
+    res.status(200).send(req.body)
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+})
